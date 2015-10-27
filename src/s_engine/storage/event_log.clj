2a2dbb030cd1f54637b85bca8e1d60311b1bffe2
dvarkin <dvarkin@gmail.com>
2015-10-27 15:33:05 +0200

    add contracts

--- src/s_engine/storage/event_log.clj
+++ src/s_engine/storage/event_log.clj
@@ -6,23 +6,28 @@
 (def ^:const event-log-table "event_log" )
 
 (defn fetch [storage event-id]
+  {:pre [(string? event-id)]
+   :post [(vector? %)]}
   (let [{:keys [conn]} storage]
     (mapv :log_item (cql/select conn event-log-table
                                 (columns :log_item)
                                 (where [[= :id event-id]])))))
 
 (defn clear! [storage event-id]
+  {:pre [(string? event-id)]}
   (let [{:keys [conn]} storage]
     (cql/delete conn event-log-table
                 (where {:id event-id}))))
 
 (defn append! [storage event-id log-rows]
+  {:pre [(string? event-id) (not-empty log-rows)]}
   (let [{:keys [conn]} storage]
     (cql/update conn event-log-table
                 (array-map :log_item (append log-rows))
                 (where [[= :id event-id]]))))
 
 (defn refresh! [storage event-id log-rows]
+  {:pre [(string? event-id) (not-empty log-rows)]}
   (let [{:keys [conn]} storage]
     (cql/insert conn event-log-table
                 {:id event-id :log_item log-rows})))
